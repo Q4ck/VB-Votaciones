cda241faf4298738c31b91a5cb93f282c70ceac7
Q4ck <quckpriv@hotmail.com>
2024-10-04 00:33:29 -0500

    first commit

---
+++ VOTACIONES/VOTACIONES/Form1.Designer.vb
@@ -0,0 +1,349 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Label2 = New Label()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        Button1 = New Button()
+        Label3 = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        Label4 = New Label()
+        CheckBox3 = New CheckBox()
+        CheckBox4 = New CheckBox()
+        CheckBox5 = New CheckBox()
+        CheckBox6 = New CheckBox()
+        Button4 = New Button()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Button5 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(98, -2)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(679, 48)
+        Label1.TabIndex = 0
+        Label1.Text = "Bienvenido Al Programa De Votacion"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), Image)
+        PictureBox1.Location = New Point(12, 97)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(230, 155)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        PictureBox1.Visible = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), Image)
+        PictureBox2.Location = New Point(263, 96)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(223, 156)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 2
+        PictureBox2.TabStop = False
+        PictureBox2.Visible = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(538, 115)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(0, 25)
+        Label2.TabIndex = 3
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(220, 231)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(22, 21)
+        CheckBox1.TabIndex = 4
+        CheckBox1.UseVisualStyleBackColor = True
+        CheckBox1.Visible = False
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(464, 231)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(22, 21)
+        CheckBox2.TabIndex = 5
+        CheckBox2.UseVisualStyleBackColor = True
+        CheckBox2.Visible = False
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(562, 180)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(159, 47)
+        Button1.TabIndex = 6
+        Button1.Text = "1. Julio Arberto"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(637, 68)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(219, 32)
+        Label3.TabIndex = 7
+        Label3.Text = "Lista De Votantes"
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(743, 180)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(143, 47)
+        Button2.TabIndex = 8
+        Button2.Text = "2. Estefania "
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(637, 253)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(159, 47)
+        Button3.TabIndex = 9
+        Button3.Text = "3. Roberto"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(578, 115)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(199, 32)
+        Label4.TabIndex = 12
+        Label4.Text = "Elige tu nombre"
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(220, 231)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(22, 21)
+        CheckBox3.TabIndex = 16
+        CheckBox3.UseVisualStyleBackColor = True
+        CheckBox3.Visible = False
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(464, 231)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(22, 21)
+        CheckBox4.TabIndex = 15
+        CheckBox4.UseVisualStyleBackColor = True
+        CheckBox4.Visible = False
+        ' 
+        ' CheckBox5
+        ' 
+        CheckBox5.AutoSize = True
+        CheckBox5.Location = New Point(220, 231)
+        CheckBox5.Name = "CheckBox5"
+        CheckBox5.Size = New Size(22, 21)
+        CheckBox5.TabIndex = 17
+        CheckBox5.UseVisualStyleBackColor = True
+        CheckBox5.Visible = False
+        ' 
+        ' CheckBox6
+        ' 
+        CheckBox6.AutoSize = True
+        CheckBox6.Location = New Point(464, 231)
+        CheckBox6.Name = "CheckBox6"
+        CheckBox6.Size = New Size(22, 21)
+        CheckBox6.TabIndex = 18
+        CheckBox6.UseVisualStyleBackColor = True
+        CheckBox6.Visible = False
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Segoe UI Black", 16F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button4.Location = New Point(12, 292)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(208, 59)
+        Button4.TabIndex = 19
+        Button4.Text = "  "
+        Button4.UseVisualStyleBackColor = True
+        Button4.Visible = False
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(69, 62)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(90, 32)
+        Label5.TabIndex = 20
+        Label5.Text = "Fabian"
+        Label5.Visible = False
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(336, 61)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(88, 32)
+        Label6.TabIndex = 21
+        Label6.Text = "Ruben"
+        Label6.Visible = False
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label7.Location = New Point(28, 377)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(90, 32)
+        Label7.TabIndex = 22
+        Label7.Text = "Fabian"
+        Label7.Visible = False
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label8.Location = New Point(28, 427)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(88, 32)
+        Label8.TabIndex = 23
+        Label8.Text = "Ruben"
+        Label8.Visible = False
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label9.Location = New Point(130, 377)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(0, 32)
+        Label9.TabIndex = 24
+        Label9.Visible = False
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label10.Location = New Point(122, 427)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(0, 32)
+        Label10.TabIndex = 25
+        Label10.Visible = False
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Segoe UI Black", 16F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button5.Location = New Point(244, 292)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(208, 59)
+        Button5.TabIndex = 26
+        Button5.Text = "Reseteo"
+        Button5.UseVisualStyleBackColor = True
+        Button5.Visible = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(920, 503)
+        Controls.Add(Button5)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Button4)
+        Controls.Add(CheckBox6)
+        Controls.Add(CheckBox5)
+        Controls.Add(CheckBox3)
+        Controls.Add(CheckBox4)
+        Controls.Add(Label4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(Label2)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Button5 As Button
+
+End Class
---
+++ VOTACIONES/VOTACIONES/Form1.vb
@@ -0,0 +1,154 @@
+﻿Imports System.Diagnostics.Contracts
+
+Public Class Form1
+    Dim num1 As Integer = 0
+    Dim num2 As Integer = 0
+    Dim num3 As Integer = 0
+    Dim num4 As Integer = 0
+    Dim num5 As Integer = 0
+    Dim num6 As Integer = 0
+    Dim a As Integer = 0
+    Dim b As Integer = 0
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then num1 = 1 Else num1 = 0
+        CheckBox2.Visible = False
+        Button1.Visible = False
+        a = num1 + num3 + num5
+        b = num2 + num4 + num6
+
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked Then num2 = 1 Else num2 = 0
+        CheckBox1.Visible = False
+        Button1.Visible = False
+        a = num1 + num3 + num5
+        b = num2 + num4 + num6
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PictureBox1.Visible = True
+        PictureBox2.Visible = True
+        CheckBox1.Visible = True
+        CheckBox2.Visible = True
+        Label5.Visible = True
+        Label6.Visible = True
+        a = num1 + num3 + num5
+        b = num2 + num4 + num6
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        CheckBox1.Visible = False
+        CheckBox2.Visible = False
+        CheckBox3.Visible = True
+        CheckBox4.Visible = True
+        PictureBox1.Visible = True
+        PictureBox2.Visible = True
+        Label5.Visible = True
+        Label6.Visible = True
+        a = num1 + num3 + num5
+        b = num2 + num4 + num6
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        If CheckBox3.Checked Then num3 = 1 Else num3 = 0
+        CheckBox4.Visible = False
+        Button2.Visible = False
+        a = num1 + num3 + num5
+        b = num2 + num4 + num6
+    End Sub
+
+    Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox4.CheckedChanged
+        If CheckBox4.Checked Then num4 = 1 Else num4 = 0
+        CheckBox3.Visible = False
+        Button2.Visible = False
+        a = num1 + num3 + num5
+        b = num2 + num4 + num6
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        CheckBox3.Visible = False
+        CheckBox4.Visible = False
+        CheckBox5.Visible = True
+        CheckBox6.Visible = True
+        PictureBox1.Visible = True
+        PictureBox2.Visible = True
+        Label5.Visible = True
+        Label6.Visible = True
+        a = num1 + num3 + num5
+        b = num2 + num4 + num6
+    End Sub
+
+    Private Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox5.CheckedChanged
+        If CheckBox5.Checked Then num5 = 1 Else num5 = 0
+        CheckBox6.Visible = False
+        Button3.Visible = False
+        Button4.Visible = True
+        a = num1 + num3 + num5
+        b = num2 + num4 + num6
+
+    End Sub
+
+    Private Sub CheckBox6_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox6.CheckedChanged
+        If CheckBox6.Checked Then num6 = 1 Else num6 = 0
+        CheckBox5.Visible = False
+        Button3.Visible = False
+        Button4.Visible = True
+        a = num1 + num3 + num5
+        b = num2 + num4 + num6
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        CheckBox5.Visible = False
+        CheckBox6.Visible = False
+        Label7.Visible = True
+        Label8.Visible = True
+        Label9.Text = a
+        Label10.Text = b
+        Label9.Visible = True
+        Label10.Visible = True
+        a = num1 + num3 + num5
+        b = num2 + num4 + num6
+        Button5.Visible = True
+        Button4.Visible = False
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Label7.Visible = False
+        Label8.Visible = False
+        Label9.Visible = False
+        Label10.Visible = False
+        a = 0
+        b = 0
+        num1 = 0
+        num2 = 0
+        num3 = 0
+        num4 = 0
+        num5 = 0
+        num6 = 0
+        Button4.Visible = False
+        Button1.Visible = True
+        Button2.Visible = True
+        Button3.Visible = True
+        Button5.Visible = False
+
+        CheckBox1.Checked = False
+        CheckBox2.Checked = False
+        CheckBox3.Checked = False
+        CheckBox4.Checked = False
+        CheckBox5.Checked = False
+        CheckBox6.Checked = False
+
+        a = num1 + num3 + num5
+        b = num2 + num4 + num6
+    End Sub
+End Class
